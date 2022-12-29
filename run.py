07eea19bd6b005bae27efbd3cfe5e52589f9af76
DanielleDaly <daniellerdaly@gmail.com>
2022-12-29 18:42:29 +0000

    Create get_player_guess function and move user input into function

--- run.py
+++ run.py
@@ -13,6 +13,15 @@ def welcome_message():
     print(f"Hi {player_name}, ready to play the game?")
 
 
+def get_player_guess():
+    try:
+        player_guess = int(input("Please choose a number between 1 and 100:"))
+    except ValueError:
+        print("Invalid data: Your guess must be a number")
+        player_guess = int(input("Please choose a number between 1 and 100:"))
+    return player_guess
+
+
 def check_guess(player_guess):
     """
     Check player guess against computer guess and display message to the user
@@ -21,10 +30,8 @@ def check_guess(player_guess):
     while computer_guess != player_guess:
         if player_guess < computer_guess:
             print("Unlucky! The correct answer is Higher")
-            player_guess = int(input("Please choose another number:"))
         elif player_guess > computer_guess:
             print("Unlucky! The correct answer is Lower")
-            player_guess = int(input("Please choose another number:"))
         else:
             break
     print("Congratulations, you win!")
@@ -34,10 +41,8 @@ welcome_message()
 
 computer_guess = random.randrange(1, 100)
 
-try:
-    player_guess = int(input("Please choose a number between 1 and 100:"))
-    check_guess(player_guess)
-except ValueError:
-    print("Invalid data: Your guess must be a number")
-    player_guess = int(input("Please choose a number between 1 and 100:"))
-    check_guess(player_guess)
+player_guess = get_player_guess()
+
+
+# if player_guess > 100:
+# # raise ValueError("Invalid data: you must choose a number less than 100")
