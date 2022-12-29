7461f7a58b6c2032c5314981255c8dfef8014870
DanielleDaly <daniellerdaly@gmail.com>
2022-12-29 15:47:43 +0000

    Generate random number, take player guess as input & check if guess is correct

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import random
+
+
 def welcome_message():
     """
     Displays welcome message to the user
@@ -11,3 +14,16 @@ def welcome_message():
 
 
 welcome_message()
+
+computer_guess = random.randrange(1, 100)
+player_guess = int(input("Please choose a number between 1 and 100:"))
+while computer_guess != player_guess:
+    if player_guess < computer_guess:
+        print("Unlucky! The correct answer is Higher")
+        player_guess = int(input("Please choose another number:"))
+    elif player_guess > computer_guess:
+        print("Unlucky! The correct answer is Lower")
+        player_guess = int(input("Please choose another number:"))
+    else:
+        break
+print("Congratulations, you win!")
