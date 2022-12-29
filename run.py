72b7d53fb01e513a159f92d9c2ea62789f264d42
DanielleDaly <daniellerdaly@gmail.com>
2022-12-29 14:53:28 +0000

    Create welcome_messsage function to add a welcome message for the user

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message():
+    """
+    Displays welcome message to the user
+    """
+    print("Welcome!")
+    print("Please choose a number between 1 and 100")
+    print("You have 5 chances to guess the correct answer\n")
+
+
+welcome_message()
