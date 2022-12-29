5dae6cd03ece053a26f03ba594535a9950dec8ae
DanielleDaly <daniellerdaly@gmail.com>
2022-12-29 15:10:25 +0000

    Take users name as input and display start game message

--- run.py
+++ run.py
@@ -6,5 +6,8 @@ def welcome_message():
     print("Please choose a number between 1 and 100")
     print("You have 5 chances to guess the correct answer\n")
 
+    player_name = input("Please enter your name:\n")
+    print(f"Hi {player_name}, ready to play the game?")
+
 
 welcome_message()
